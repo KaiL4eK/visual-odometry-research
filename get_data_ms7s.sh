26ef25d200a41b095491380ceaa6ffe68e90c2d6
Alex (quadro) <alex_quadro@etu.ru>
2020-04-13 17:58:31 +0300

    Data scripts

---
+++ get_data_ms7s.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+mkdir -p data/MS7Scenes; cd $_
+wget -N http://download.microsoft.com/download/2/8/5/28564B23-0828-408F-8631-23B1EFF1DAC8/chess.zip
+wget -N http://download.microsoft.com/download/2/8/5/28564B23-0828-408F-8631-23B1EFF1DAC8/fire.zip
+wget -N http://download.microsoft.com/download/2/8/5/28564B23-0828-408F-8631-23B1EFF1DAC8/heads.zip
+wget -N http://download.microsoft.com/download/2/8/5/28564B23-0828-408F-8631-23B1EFF1DAC8/office.zip
+wget -N http://download.microsoft.com/download/2/8/5/28564B23-0828-408F-8631-23B1EFF1DAC8/pumpkin.zip
+wget -N http://download.microsoft.com/download/2/8/5/28564B23-0828-408F-8631-23B1EFF1DAC8/redkitchen.zip
+wget -N http://download.microsoft.com/download/2/8/5/28564B23-0828-408F-8631-23B1EFF1DAC8/stairs.zip
