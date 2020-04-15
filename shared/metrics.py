1da1be6811d9f75e25c15fa206e5d37fe72482ee
Alex (quadro) <alex_quadro@etu.ru>
2020-04-15 23:46:15 +0300

    Added sequence metrics

--- shared/metrics.py
+++ shared/metrics.py
@@ -43,3 +43,77 @@ def compute_RPE(gt, pred):
     rpe_trans = np.mean(trans_errors)
     rpe_rot = np.mean(rot_errors)
     return rpe_trans, rpe_rot
+
+
+def trajectory_distances(poses):
+    diffs = np.diff(poses[:,:3,3], axis=0)
+    deltas = np.linalg.norm(diffs, axis=1)
+    dists = np.cumsum([0, *deltas])
+    return dists
+
+
+def last_frame_from_segment_length(dists, first_frame, length):
+    for i in range(first_frame, len(dists)):
+        if dists[i] > (dists[first_frame] + length):
+            return i
+    return -1
+
+
+def calc_sequence_errors(poses_gt, poses_result):
+    err = []
+    dists = trajectory_distances(poses_gt)
+    step_size = 10
+    lengths = [100, 200, 300, 400, 500, 600, 700, 800]
+    
+    for first_frame_idx in range(0, len(poses_gt), step_size):
+        for len_ in lengths:
+            last_frame_idx = last_frame_from_segment_length(
+                                    dists, first_frame_idx, len_
+                                    )
+            if last_frame_idx == -1:
+                continue
+
+            pose_delta_gt = np.dot(
+                                np.linalg.inv(poses_gt[first_frame_idx]),
+                                poses_gt[last_frame_idx]
+                                )
+            pose_delta_result = np.dot(
+                                    np.linalg.inv(poses_result[first_frame_idx]),
+                                    poses_result[last_frame_idx]
+                                    )
+            pose_error = np.dot(
+                            np.linalg.inv(pose_delta_result),
+                            pose_delta_gt
+                            )
+
+            r_err = rotation_error(pose_error)
+            t_err = translation_error(pose_error)
+
+            # compute speed
+            num_frames = last_frame_idx - first_frame_idx + 1.0
+            speed = len_/(0.1*num_frames)
+
+            err.append(
+                (first_frame_idx, r_err/len_, t_err/len_, len_, speed)
+            )
+    return np.array(err)
+
+
+def compute_segment_error(seq_errs):
+    avg_segment_errs = {}
+    lengths = np.unique(seq_errs[:,3])
+        
+    for len_ in lengths:
+        data = seq_errs[seq_errs[:,3] == len_]
+        avg = np.mean(data[:, 1:3], axis=0)
+        avg_segment_errs[len_] = avg
+
+    return avg_segment_errs
+
+
+def compute_overall_err(seq_errs):
+    if seq_errs.shape[0] > 0:
+        return np.mean(seq_errs[:, 1:3], axis=0)
+    else:
+        return 0, 0
+    
