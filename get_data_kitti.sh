1553bd8301d8a7c568fa2550812b7df0748be691
Alex (quadro) <alex_quadro@etu.ru>
2020-05-05 23:17:40 +0300

    Start working on manual visual odometry

--- get_data_kitti.sh
+++ get_data_kitti.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir -p data/KITTI; cd $_
-wget -N http://download.microsoft.com/download/2/8/5/28564B23-0828-408F-8631-23B1EFF1DAC8/chess.zip
+wget -N https://s3.eu-central-1.amazonaws.com/avg-kitti/data_odometry_color.zip
 wget -N https://s3.eu-central-1.amazonaws.com/avg-kitti/data_odometry_poses.zip
 wget -N https://s3.eu-central-1.amazonaws.com/avg-kitti/devkit_odometry.zip
+wget -N https://s3.eu-central-1.amazonaws.com/avg-kitti/data_odometry_calib.zip
